78f44ff8fc970496ae7633d4d7e2b5533424e32e
Brian Lalonde <brianary@gmail.com>
2026-07-11 16:24:38 -0700

    🧱 Change repo structure; reuse action

--- test/HtmlToMarkdown.Tests.ps1
+++ test/HtmlToMarkdown.Tests.ps1
@@ -1,6 +1,6 @@
 # Pester tests, see https://github.com/Pester/Pester/wiki
-Import-LocalizedData -BindingVariable manifest -BaseDirectory ./src/* -FileName (Split-Path $PWD -Leaf)
-$psd1 = Resolve-Path ./src/*/bin/*/net*/publish/*.psd1
+$psd1 = Resolve-Path ./src/bin/*/net*/publish/*.psd1
+Import-LocalizedData -BindingVariable manifest -BaseDirectory (Split-Path $psd1) -FileName (Split-Path $psd1 -Leaf)
 if(1 -lt ($psd1 |Measure-Object).Count) {throw "Too many module binaries found: $psd1"}
 $module = Import-Module "$psd1" -PassThru -vb
 $eol = [Environment]::NewLine
@@ -17,11 +17,11 @@ Describe $module.Name {
 		It "Given the module, the DLL product version should match the manifest version" {
 			(Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersionRaw |
 				Should -BeLike "$($manifest.ModuleVersion)*"
-		} -Pending
+		}
 		It "Given the module, the DLL should have a valid semantic product version" {
 			$v = (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersion
 			[semver]::TryParse($v, [ref]$null) |Should -BeTrue
-		} -Pending
+		}
 	}
 	Context 'Convert-HtmlToMarkdown cmdlet' -Tag Cmdlet,Convert-HtmlToMarkdown {
 		It "Given HTML '<Html>', '<Expected>' should be returned." -TestCases @(
