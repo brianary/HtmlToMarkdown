7b474f5f1a1efc5215fbfd390be3370d526542b0
Brian Lalonde <brianary@gmail.com>
2026-07-18 21:06:53 -0700

    ✅ Remove version checks

--- test/HtmlToMarkdown.Tests.ps1
+++ test/HtmlToMarkdown.Tests.ps1
@@ -6,23 +6,6 @@ $module = Import-Module "$psd1" -PassThru -vb
 $eol = [Environment]::NewLine
 
 Describe $module.Name {
-	Context "$($module.Name) module" -Tag Module {
-		It "Given the module, the version should match the manifest version" {
-			$module.Version |Should -BeExactly $manifest.ModuleVersion
-		}
-		It "Given the module, the DLL file version should match the manifest version" {
-			(Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.FileVersionRaw |
-				Should -BeLike "$($manifest.ModuleVersion)*"
-		}
-		It "Given the module, the DLL product version should match the manifest version" {
-			(Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersionRaw |
-				Should -BeLike "$($manifest.ModuleVersion)*"
-		}
-		It "Given the module, the DLL should have a valid semantic product version" {
-			$v = (Get-Item "$($module.ModuleBase)\$($module.Name).dll").VersionInfo.ProductVersion
-			[semver]::TryParse($v, [ref]$null) |Should -BeTrue
-		}
-	}
 	Context 'Convert-HtmlToMarkdown cmdlet' -Tag Cmdlet,Convert-HtmlToMarkdown {
 		It "Given HTML '<Html>', '<Expected>' should be returned." -TestCases @(
 			@{ Html = '<p>Hello, world</p>'; Expected = 'Hello, world' }
